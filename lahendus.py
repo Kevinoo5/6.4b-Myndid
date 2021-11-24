1574970438fee033a9e1b26ec6abac814def259f
Kevinoo5 <87882945+Kevinoo5@users.noreply.github.com>
2021-11-24 23:50:54 +0200

    Esimene proov, sama veaga
    Eelmisel oli for loop funktsiooni sees, aga nüüd on see väljaspool funktsiooni, ei näinud mingit vahet, aga uuendan ikka.

--- lahendus.py
+++ lahendus.py
@@ -3,11 +3,12 @@ fail = open(inp)
 mündid = []
 
 def pronksikarva_summa(mündid):
-    for münt in fail:
-        if int(münt) <= 5:
-            mündid.append(int(münt))
     summa = (sum(mündid))
     return summa
 
+for münt in fail:
+    if int(münt) <= 5:
+        mündid.append(int(münt))
+    
 print(pronksikarva_summa(mündid))
 fail.close()
